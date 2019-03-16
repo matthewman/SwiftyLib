f7573edb6eb6421fbb9d28ca9f1aa513c330eb85
Matthew Man <matthewman9639@gmail.com>
2019-03-16 16:53:18 +0800

    Added test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Matthew Man on 16/3/2019.
+//  Copyright © 2019 MatthewApp. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
